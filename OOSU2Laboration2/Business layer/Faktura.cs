afd284ec190dbb028336e91c442b55e30147bc0e
Phyrexiian <s182240@student.hb.se>
2020-01-31 11:26:46 +0100

    .

--- OOSU2Laboration2/Business layer/Faktura.cs
+++ OOSU2Laboration2/Business layer/Faktura.cs
@@ -6,5 +6,26 @@ namespace Business_layer
 {
     class Faktura
     {
-    }
+		private string totalpris;
+		public string Totalpris
+		{
+			get { return totalpris; }
+			set { totalpris = value; }
+		}
+
+		private DateTime starttid;
+		public DateTime Starttid
+		{
+			get { return starttid; }
+			set { starttid = value; }
+		}
+
+		private DateTime sluttid;
+		public DateTime Sluttid
+		{
+			get { return sluttid; }
+			set { sluttid = value; }
+		}
+
+	}
 }
